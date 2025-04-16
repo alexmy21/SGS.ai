77ad0a11c59d7f0c0b170a74d28808df1736a79c
Alex Mylnikov <alexmy@lisa-park.com>
2025-04-16 18:11:37 -0400

    fix for test_api.sh

--- test_api.sh
+++ test_api.sh
@@ -8,11 +8,11 @@ curl -X GET "$BASE_URL/" -H "Content-Type: application/json" -w "\nStatus: %{htt
 echo "Testing HDF5 processor..."
 curl -X POST "$BASE_URL/process" \
      -H "Content-Type: application/json" \
-     -d '{"transformer":"C","processor":"call_hdf5","input_sha_id":"input_sha_id_123","processor_sha_id":"processor_sha_id_456","output_sha_id":"output_sha_id_789"}' \
+     -d '{"transformer":"C","processor":"meta_hdf5.call_hdf5","input_sha_id":"input_sha_id_123","processor_sha_id":"processor_sha_id_456","output_sha_id":"output_sha_id_789"}' \
      -w "\nStatus: %{http_code}\n\n"
 
 echo "Testing Redis processor..."
 curl -X POST "$BASE_URL/process" \
      -H "Content-Type: application/json" \
-     -d '{"transformer":"C","processor":"ping_redis","input_sha_id":"input_sha_id_123","processor_sha_id":"processor_sha_id_456","output_sha_id":"output_sha_id_789"}' \
+     -d '{"transformer":"C","processor":"meta_redis.ping_redis","input_sha_id":"input_sha_id_123","processor_sha_id":"processor_sha_id_456","output_sha_id":"output_sha_id_789"}' \
      -w "\nStatus: %{http_code}\n"
